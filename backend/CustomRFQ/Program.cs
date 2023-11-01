683bde1be12ab5505ec4330af23380e628409f9f
math-s-sousa <matheus.silvasousa@hotmail.com>
2023-11-01 18:51:39 -0300

    implementado autenticação basica no backend

--- backend/CustomRFQ/Program.cs
+++ backend/CustomRFQ/Program.cs
@@ -1,5 +1,7 @@
 using CustomRFQ.Databases;
 using CustomRFQ.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors();
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton<Context>();
+
+builder.Services.AddAuthentication("BasicAuthentication")
+        .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", options => { });
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 var app = builder.Build();
@@ -16,10 +22,13 @@ app.UseCors(builder => builder
 .AllowAnyMethod()
 .AllowAnyHeader());
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/rfq/{id}", (string id, Context context) =>
+app.MapGet("/rfq/{id}", [Authorize] (string id, Context context) =>
 {
 	try
 	{
@@ -39,7 +48,7 @@ app.MapGet("/rfq/{id}", (string id, Context context) =>
 	}
 });
 
-app.MapPost("/rfq/{id}", (string id, ServiceLayer.MarketingDocument.Value body, Context context) =>
+app.MapPost("/rfq/{id}", [Authorize] (string id, ServiceLayer.MarketingDocument.Value body, Context context) =>
 {
 	try
 	{
@@ -56,19 +65,6 @@ app.MapPost("/rfq/{id}", (string id, ServiceLayer.MarketingDocument.Value body, 
 
 		else
 			return Results.Problem(patchQuotation.failed.error.message.value);
-	}
-	catch (Exception ex)
-	{
-		return Results.Problem(ex.Message);
-	}
-});
-
-app.MapPost("/migrate", () => {
-	try
-	{
-		//new Migrate(_sL);
-
-		return Results.NoContent();
 	}
 	catch (Exception ex)
 	{
---
+++ backend/CustomRFQ/Services/BasicAuthenticationHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    IConfiguration _conf;
+
+    public BasicAuthenticationHandler(
+        IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock,
+        IConfiguration conf)
+        : base(options, logger, encoder, clock)
+    {
+        _conf = conf;
+    }
+
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (!Request.Headers.ContainsKey("Authorization"))
+        {
+            return AuthenticateResult.Fail("Authorization header is missing.");
+        }
+
+        var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+
+        if (authHeader.Scheme.Equals("Basic", StringComparison.OrdinalIgnoreCase))
+        {
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+            var username = credentials[0];
+            var password = credentials[1];
+
+            // Implement your custom authentication logic here.
+            if (IsValidUser(username, password))
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, username) };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                return AuthenticateResult.Success(ticket);
+            }
+        }
+
+        return AuthenticateResult.Fail("Invalid credentials.");
+    }
+
+    private bool IsValidUser(string username, string password)
+    {
+        if (username == _conf.GetValue<string>("BasicAuth:Username") && password == _conf.GetValue<string>("BasicAuth:Password"))
+            return true;
+
+        else return false;
+    }
+}
