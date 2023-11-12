3b317ad6b2d678a2c4c8bc97037d5fa89639a767
math-s-sousa <matheus.silvasousa@hotmail.com>
2023-11-12 20:58:40 -0300

    Update Database.cs
    adicionado mapeamento de campo de UserCode na captura do Evento

--- backend/CustomRFQ/Models/Database.cs
+++ backend/CustomRFQ/Models/Database.cs
@@ -13,6 +13,7 @@ namespace CustomRFQ.Models
             public DateTime CreateDate { get; set; }
             public DateTime UpdateDate { get; set; }
             public char Status { get; set; }
+            public string UserCode { get; set; }
         }
 
         public class Config
